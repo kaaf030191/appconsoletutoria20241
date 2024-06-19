affc277782ce3911320dbec421c6e05533123c1f
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-06-18 22:50:51 -0500

    Feat: 18-06-2024 - Creación de clase.
    Manejo de clases y atributos.

---
+++ src/main/java/com/codideep/app/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codideep.app;
+
+import com.codideep.app.entity.Person;
+
+/**
+ *
+ * @author KAAF0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
---
+++ src/main/java/com/codideep/app/entity/Person.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codideep.app.entity;
+
+/**
+ *
+ * @author KAAF0
+ */
+public class Person {
+    private String firstName;
+    private String surName;
+    private String dni;
+    private String birthDate;
+    private String gender;
+}
